b12d8a3a612e3f90ca4260756ed9423c4c1a339e
Mohammad-Minaz <46309131+Mohammad-Minaz@users.noreply.github.com>
2021-02-04 22:50:14 +0530

    Create Vm-StartByTag.ps1

---
+++ Vm-StartByTag.ps1
@@ -0,0 +1,52 @@
+workflow ShutDownStartByTag 
+{ 
+        Param( 
+        [Parameter(Mandatory=$true)] 
+        [String] 
+        $TagName, 
+        [Parameter(Mandatory=$true)] 
+        [String] 
+        $TagValue, 
+        [Parameter(Mandatory=$true)] 
+        [Boolean] 
+        $Shutdown 
+        ) 
+      
+    $connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+ 
+    "Logging in to Azure..."
+    Connect-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch 
+{
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+} 
+    $vms = Get-AzResource -TagName $TagName -TagValue $TagValue | where {$_.ResourceType -like "Microsoft.Compute/virtualMachines"} 
+      
+    Foreach -Parallel ($vm in $vms){ 
+         
+        if($Shutdown){ 
+            Write-Output "Stopping $($vm.Name)";         
+            Stop-AzVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -Force; 
+        } 
+        else{ 
+            Write-Output "Starting $($vm.Name)";         
+            Start-AzVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName; 
+        } 
+    } 
+}
